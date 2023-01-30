deb2c06b2fe57accf03028a46de448d89dba1bde
Ewen35550 <ewen.herve.35550@gmail.com>
2023-01-30 09:24:32 +0100

    feat: ajout des Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@gmail.com');
+        $user->setName('Admin');
+        $user->setPassword($this->hasher->hashPassword($user, 'k8i8WV3Ua2Yi2w'));
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
