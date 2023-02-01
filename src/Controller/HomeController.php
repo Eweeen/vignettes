d88bc1c283261bdb15a27c3719dc8065dd390966
Ewen35550 <ewen.herve.35550@gmail.com>
2023-02-01 11:08:02 +0100

    feat: ajout des medias dans la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MediasRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(MediasRepository $mediasRepository): Response
     {
+        $medias = $mediasRepository->findBy(array('is_active' => true), array('id' => 'DESC'));
+
         return $this->render('home/index.html.twig', [
             'user' => $this->getUser(),
+            'medias' => $medias,
         ]);
     }
 }
