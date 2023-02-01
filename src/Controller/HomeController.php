78da2e138ffa7c6f856b38bc999240b5d859fa22
Ewen35550 <ewen.herve.35550@gmail.com>
2023-02-01 12:19:59 +0100

    feat: ajout des filtres par utilisateurs et par categories

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,66 @@
 namespace App\Controller;
 
 use App\Repository\MediasRepository;
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(MediasRepository $mediasRepository): Response
+    public function index(Request $request, MediasRepository $mediasRepository, CategoriesRepository $categoriesRepository): Response
     {
-        $medias = $mediasRepository->findBy(array('is_active' => true), array('id' => 'DESC'));
+        if ($request->isMethod('post') && intval($request->get('category_filter'), 10) !== 0) {
+            $mediaSelected = $request->get('category_filter');
+            $medias = $mediasRepository->findBy(
+                array('category' => $request->get('category_filter')),
+                array('id' => 'DESC'),
+                array('is_active' => true)
+            );
+        } else {
+            $mediaSelected = 0;
+            $medias = $mediasRepository->findBy(
+                array('is_active' => true),
+                array('id' => 'DESC')
+            );
+        }
+
+        $categories = $categoriesRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'user' => $this->getUser(),
             'medias' => $medias,
+            'mediaSelected' => $mediaSelected,
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/posts/{id}', name: 'app_posts')]
+    public function userPost($id, Request $request, MediasRepository $mediasRepository, CategoriesRepository $categoriesRepository): Response
+    {
+        if ($request->isMethod('post') && intval($request->get('category_filter'), 10) !== 0) {
+            $mediaSelected = $request->get('category_filter');
+            $medias = $mediasRepository->findBy(
+                array('user' => $id, 'category' => $request->get('category_filter')),
+                array('id' => 'DESC'),
+            );
+        } else {
+            $mediaSelected = 0;
+            $medias = $mediasRepository->findBy(
+                array('user' => $id),
+                array('id' => 'DESC'),
+            );
+        }
+
+        $categories = $categoriesRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'user' => $this->getUser(),
+            'medias' => $medias,
+            'mediaSelected' => $mediaSelected,
+            'categories' => $categories,
         ]);
     }
 }
