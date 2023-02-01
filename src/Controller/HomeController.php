4a85f4931726faa3fc88de73eec0968af5220262
Ewen35550 <ewen.herve.35550@gmail.com>
2023-02-01 16:24:36 +0100

    refactor: fix des bugs et meilleure lisibilité du code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,58 +11,67 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(Request $request, MediasRepository $mediasRepository, CategoriesRepository $categoriesRepository): Response
-    {
-        if ($request->isMethod('post') && intval($request->get('category_filter'), 10) !== 0) {
-            $mediaSelected = $request->get('category_filter');
-            $medias = $mediasRepository->findBy(
-                array('category' => $request->get('category_filter')),
-                array('id' => 'DESC'),
-                array('is_active' => true)
-            );
-        } else {
-            $mediaSelected = 0;
-            $medias = $mediasRepository->findBy(
-                array('is_active' => true),
-                array('id' => 'DESC')
-            );
-        }
-
-        $categories = $categoriesRepository->findAll();
-
-        return $this->render('home/index.html.twig', [
-            'user' => $this->getUser(),
-            'medias' => $medias,
-            'mediaSelected' => $mediaSelected,
-            'categories' => $categories,
-        ]);
+  #[Route('/', name: 'app_home')]
+  public function index(Request $request, MediasRepository $mediasRepository, CategoriesRepository $categoriesRepository): Response
+  {
+    if ($request->isMethod('post') && intval($request->get('category_filter'), 10) !== 0) {
+      $mediaSelected = $request->get('category_filter');
+      $medias = $mediasRepository->findBy(
+        array(
+          'category' => $request->get('category_filter'),
+          'is_active' => true
+        ),
+        array('id' => 'DESC')
+      );
+    } else {
+      $mediaSelected = 0;
+      $medias = $mediasRepository->findBy(
+        array('is_active' => true),
+        array('id' => 'DESC')
+      );
     }
 
-    #[Route('/posts/{id}', name: 'app_posts')]
-    public function userPost($id, Request $request, MediasRepository $mediasRepository, CategoriesRepository $categoriesRepository): Response
-    {
-        if ($request->isMethod('post') && intval($request->get('category_filter'), 10) !== 0) {
-            $mediaSelected = $request->get('category_filter');
-            $medias = $mediasRepository->findBy(
-                array('user' => $id, 'category' => $request->get('category_filter')),
-                array('id' => 'DESC'),
-            );
-        } else {
-            $mediaSelected = 0;
-            $medias = $mediasRepository->findBy(
-                array('user' => $id),
-                array('id' => 'DESC'),
-            );
-        }
+    $categories = $categoriesRepository->findAll();
 
-        $categories = $categoriesRepository->findAll();
+    return $this->render('home/index.html.twig', [
+      'user' => $this->getUser(),
+      'medias' => $medias,
+      'mediaSelected' => $mediaSelected,
+      'categories' => $categories,
+    ]);
+  }
 
-        return $this->render('home/index.html.twig', [
-            'user' => $this->getUser(),
-            'medias' => $medias,
-            'mediaSelected' => $mediaSelected,
-            'categories' => $categories,
-        ]);
+  #[Route('/posts/{id}', name: 'app_posts')]
+  public function userPost($id, Request $request, MediasRepository $mediasRepository, CategoriesRepository $categoriesRepository): Response
+  {
+    if ($request->isMethod('post') && intval($request->get('category_filter'), 10) !== 0) {
+      $mediaSelected = $request->get('category_filter');
+      $medias = $mediasRepository->findBy(
+        array(
+          'user' => $id,
+          'category' => $request->get('category_filter'),
+          'is_active' => true
+        ),
+        array('id' => 'DESC'),
+      );
+    } else {
+      $mediaSelected = 0;
+      $medias = $mediasRepository->findBy(
+        array(
+          'user' => $id,
+          'is_active' => true
+        ),
+        array('id' => 'DESC'),
+      );
     }
+
+    $categories = $categoriesRepository->findAll();
+
+    return $this->render('home/index.html.twig', [
+      'user' => $this->getUser(),
+      'medias' => $medias,
+      'mediaSelected' => $mediaSelected,
+      'categories' => $categories,
+    ]);
+  }
 }
